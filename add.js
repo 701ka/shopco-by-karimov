13ab1aab2758cf5134f195506e9f8ee140792ea6
701ka <muslimakarimova73@gmail.com>
2026-04-08 23:09:17 +0500

    FAh

--- add.js
+++ add.js
@@ -1,6 +1,26 @@
 const btn = document.querySelector(".add__btn_modal");
 const modal = document.getElementById("modal");
+const form = document.querySelector(".add__form");
 
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+});
 btn.addEventListener("click", () => {
   modal.classList.toggle("active");
+});
+
+document.addEventListener("click", (e) => {
+  const isClickInsideModal = e.target.closest(".add__modal_content");
+  const isOpenBtn = e.target.closest(".add__btn_modal");
+
+  if (!isClickInsideModal && !isOpenBtn) {
+    modal.classList.remove("active");
+  }
+});
+const iconWrapper = document.querySelectorAll(".icon__inner");
+
+iconWrapper.forEach((el) => {
+  el.addEventListener("click", () => {
+    alert("bosildi");
+  });
 });
