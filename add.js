5fcdfef9536b7b5209e07d5111ceda930e2cc3ff
701ka <muslimakarimova73@gmail.com>
2026-04-09 10:09:05 +0500

    error alert fixed

--- add.js
+++ add.js
@@ -1,26 +1,31 @@
 const btn = document.querySelector(".add__btn_modal");
 const modal = document.getElementById("modal");
 const form = document.querySelector(".add__form");
-
+const iconWrapper = document.querySelectorAll(".icon__inner");
+const ProductList = document.querySelector(".add__product");
+const delteModal = document.querySelector(".delete__alert");
+const modalYes = document.querySelector("#delete");
+const modalNo = document.querySelector("#noDelete");
 form.addEventListener("submit", (e) => {
   e.preventDefault();
 });
 btn.addEventListener("click", () => {
-  modal.classList.toggle("active");
+  modal.classList.add("active");
 });
 
 document.addEventListener("click", (e) => {
-  const isClickInsideModal = e.target.closest(".add__modal_content");
-  const isOpenBtn = e.target.closest(".add__btn_modal");
+  const modalIn = e.target.closest(".add__modal_content");
+  const modalBtn = e.target.closest(".add__btn_modal");
 
-  if (!isClickInsideModal && !isOpenBtn) {
+  if (!modalIn && !modalBtn) {
     modal.classList.remove("active");
   }
 });
-const iconWrapper = document.querySelectorAll(".icon__inner");
-
-iconWrapper.forEach((el) => {
-  el.addEventListener("click", () => {
-    alert("bosildi");
-  });
+ProductList.addEventListener("click", (e) => {
+  if (e.target.closest(".icon__inner")) {
+    delteModal.style.display = "flex  ";
+  }
+});
+modalNo.addEventListener("click", () => {
+  delteModal.style.display = "none";
 });
