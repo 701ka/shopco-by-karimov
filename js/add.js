8dbb728d5e95f81dbfd6283b9e46f550238832af
701ka <muslimakarimova73@gmail.com>
2026-04-14 10:42:44 +0500

    admin panel changed

--- js/add.js
+++ js/add.js
@@ -6,6 +6,51 @@ const ProductList = document.querySelector(".add__product");
 const delteModal = document.querySelector(".delete__alert");
 const modalYes = document.querySelector("#delete");
 const modalNo = document.querySelector("#noDelete");
+
+// list
+const productListEl = document.querySelector(".add__product__list");
+
+async function getProduct() {
+  const res = await fetch(
+    "https://shop-co-backend-k5f0.onrender.com/api/products",
+  );
+  const data = await res.json();
+  productsRender(data, productListEl);
+}
+getProduct();
+function productsRender(arr, list) {
+  list.innerHTML = arr
+    .map(
+      (el, index) => `
+     <li class="add__product__item">
+            <p class="add__product__number">${index + 1}</p>
+            <img
+              src="${el.images[0]}"
+              width="100"
+              height="88"
+              alt=""
+              class="add__product__img"
+            />
+            <p class="add__product__name">${el.title}</p>
+            <p class="add__product__price">${el.price}</p>
+            <p class="add__product__discount">-20%</p>
+            <div class="add__product__star_wrapper">
+              <p class="add__product__star_counter">5</p>
+              <i class="fa-solid fa-star add__product__star_icon"></i>
+            </div>
+            <div class="add__product_icon_wrapper">
+              <div class="icon__inner">
+                <i class="fa-solid fa-pencil add__product_icon pencil"></i>
+              </div>
+              <div class="icon__inner trash_wrapper icon__delete">
+                <i class="fa-solid fa-trash add__product_icon trash"></i>
+              </div>
+            </div>
+          </li>`,
+    )
+    .join("");
+}
+
 form.addEventListener("submit", (e) => {
   e.preventDefault();
 });
@@ -22,7 +67,7 @@ document.addEventListener("click", (e) => {
   }
 });
 ProductList.addEventListener("click", (e) => {
-  if (e.target.closest(".icon__inner")) {
+  if (e.target.closest(".icon__delete")) {
     delteModal.style.display = "flex  ";
   }
 });
